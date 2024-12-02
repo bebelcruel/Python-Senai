248043d084a56642c3dae251908cfcf2b6d14487
bebelcruel <abelclaudino092@gmail.com>
2024-12-02 10:01:46 -0300

    Add files via upload
    Aprendi o que é uma variável, comentário, e também o conceito da tabela verdade.

---
+++ Aula 2-Python/Aula 2.py
@@ -0,0 +1,9 @@
+print("joao é um fi da mae")
+
+nome_usuario = 'belaosempaixao'#tipo -> str = string
+minha_idade = 19#tipo -> int = interger
+minha_altura = 1.68#tipo -> = float
+maior_de_idade = True#tipo -> bool = boolean
+menor_de_idade = False#tipo -> bool = boolean
+
+print('Eu sou',nome_usuario,'Minha idade é', minha_idade, 'minha altura é',minha_altura, 'eu sou maior de idade?',maior_de_idade)
